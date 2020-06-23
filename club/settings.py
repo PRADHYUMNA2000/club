158bb7dada2e1913f2bdf33b367319ddb6404397
Pradhyumna Rao <pradhyumnarao2000@gmail.com>
2020-06-23 07:55:54 +0530

    initial commit

--- club/settings.py
+++ club/settings.py
@@ -36,7 +36,6 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'club.urls'
@@ -117,4 +116,3 @@ STATICFILES_DIRS = (
 
 STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles')
 
-STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
